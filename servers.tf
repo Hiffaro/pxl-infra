314fd4336fb8668491d047fd22397616351b47e6
Christophe van der Puijl <hiffaro@gmail.com>
2024-05-08 15:40:46 +0200

    fixed a typo

--- servers.tf
+++ servers.tf
@@ -28,9 +28,9 @@ locals {
       ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILe8frS9W779nR/TVBTc1giVLt2bwfTYnfqMt7D2qozB 12300247@student.pxl.be",
       ]
-          Christophe = {
+    Christophe = {
       ssh_keys = [
-        ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/TzK2ORvoOQXWLWnQQcHoEuDY7PUlp051mwiaQpzeQ pandora@gmail.com",
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/TzK2ORvoOQXWLWnQQcHoEuDY7PUlp051mwiaQpzeQ pandora@gmail.com",
       ]
     }
     }
